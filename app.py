cd6ab3719d61619b52b4e0cc8032b08d2fddfb56
Emma Victoria <v1ckyk4t_a@proton.me>
2023-12-05 21:04:26 -0300

    minor bug fixes

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from os.path import join, getsize
 
 VIDEO_CODEC = 'hevc_nvenc' #this is for nvidia, for CPU u can specify libx265
 
-def transcod(video, out, new_bitrate):
+def transcod(video, out, new_bitrate, audio_codec, audio_bitrate):
     
     (
     ffmpeg
     .input(video)
-    .output(out, **{'b:v': new_bitrate, 'codec:v':VIDEO_CODEC, 'preset': 'slow'})
+    .output(out, **{'b:v': new_bitrate, 'codec:v':VIDEO_CODEC, 'preset': 'slow', 'b:a':audio_bitrate, 'codec:a': audio_codec})
     .run()
     )
     print("[ OK ]")
@@ -30,14 +30,16 @@ def main():
                     
                     bit_rate = metadata['streams'][0]['bit_rate']
                     new_bitrate = int(float(bit_rate) * .60) #60%
-                    
                 
+                    audio_codec = metadata['streams'][1]['codec_name']
+                    audio_bitrate = metadata['streams'][1]['bit_rate']
+
                     if(codec == 'h264'):
                         out = os.path.join(root, "h265_" + name)
                         print("transcoding " + video)
-                        transcod(video, out, new_bitrate)
+                        transcod(video, out, new_bitrate, audio_codec, audio_bitrate)
                     else:
-                        print("ignoring " + name + " (NOT H.264)")
-                     
+                        print("ignoring " + name + " (NOT H.264)") 
+
 main()
 
